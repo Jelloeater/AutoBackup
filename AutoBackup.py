6697b1f7b8b4f4b5b30d1e67e12329b699abdd9b
Jesse <jelloeater@gmail.com>
2017-07-16 15:26:06 -0400

    Started working on password helper class

--- AutoBackup.py
+++ AutoBackup.py
@@ -1,8 +1,7 @@
-import os
+import base64
 import keyring
 import argparse
 import logging
-import datetime
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -17,6 +16,33 @@ logging.basicConfig(format="[%(asctime)s] [%(levelname)8s] --- %(message)s (%(fi
 BASE = declarative_base()  # Needs to be module level w/ database
 
 
+class PasswordHelper:
+    @staticmethod
+    def setup_master_key():
+        logging.debug("Setting up master key")
+        key = Fernet.generate_key()
+        logging.debug(key)
+        encode_key = base64.urlsafe_b64encode(key)
+        logging.debug(encode_key)
+        keyring.set_password("AutoBackup", "DBkey", encode_key)
+
+    @staticmethod
+    def get_master_pass():
+        key_pass = keyring.get_password("AutoBackup", "DBkey")
+        decode_key_pass = base64.urlsafe_b64decode(key_pass)
+        return decode_key_pass
+
+    @staticmethod
+    def decode_password(hash_pass_in):
+        f = Fernet(PasswordHelper.get_master_pass())
+        return f.decrypt(hash_pass_in)
+
+    @staticmethod
+    def encode_password(password_in):
+        f = Fernet(PasswordHelper.get_master_pass())
+        return f.encrypt(password_in)
+
+
 class DatabaseHelper:
     # from sqlalchemy.dialects.sqlite import \
     # BLOB, BOOLEAN, CHAR, DATE, DATETIME, DECIMAL, FLOAT, \
@@ -29,7 +55,7 @@ class DatabaseHelper:
         print('Are you sure to want to DROP ALL TABLES, this cannot be undone!')
         if input('({0})>'.format('Type YES to continue')) == 'YES':
             logging.info("Resetting master key")
-            keyring.set_password("AutoBackup", "DBkey", Fernet.generate_key())
+
             BASE.metadata.drop_all(engine)
             BASE.metadata.create_all(engine)
             print('DATABASE RE-INITIALIZED')
@@ -49,48 +75,29 @@ class DatabaseHelper:
 
     def get_all_rows(self):
         session = self.get_session()
-        return session.query(EnvData).all()
+        return session.query(Database_ORM).all()
 
-    def get_last_x_rows(self, x):
-        session = self.get_session()
-        return session.query(EnvData).order_by(EnvData.row_id.desc()).limit(x).all()
-
-    def add_data(self, data_obj, client_ip):
-
-        # Create DB obj class instance
-        db_entry = EnvData()
-        db_entry.client_ip = client_ip
-        db_entry.timestamp = datetime.datetime.now().isoformat()
-        db_entry.altitude = data_obj['altitude']
-        db_entry.p = data_obj['p']
-        db_entry.temp = data_obj['temp']
-
-        # Write DB obj to disk
+    def add_data(self):
+        db_entry = Database_ORM()  # Create DB obj class instance
+        db_entry.ssh_ip = input('Enter ssh_ip:')
+        db_entry.ssh_username = input('Enter ssh_username:')
+        db_entry.ssh_password = PasswordHelper.encode_password(input('Enter ssh_password:'))
+        db_entry.ssh_command = input('Enter ssh_command:')
         s = self.get_session()
         s.add(db_entry)
-        s.commit()
+        s.commit()  # Write DB obj to disk
 
-    @staticmethod
-    def get_password(hash_pass_in):
-        f = Fernet(keyring.get_password("AutoBackup", "DBkey"))
-        return f.decrypt(hash_pass_in)
-
-    @staticmethod
-    def set_password(password_in):
-        f = Fernet(keyring.get_password("AutoBackup", "DBkey"))
-        return f.encrypt(password_in)
 
 # Classes are directly mapped to tables, without the need for a mapper binding (ex mapper(Class, table_definition))
-class EnvData(BASE):
+class Database_ORM(BASE):
     from sqlalchemy import Column, Integer, String
     """Defines Device object relational model, is used for both table creation and object interaction"""
-    __tablename__ = 'EnvData'
+    __tablename__ = 'Database_ORM'
     row_id = Column('row_id', Integer, primary_key=True)
-    timestamp = Column('timestamp', String)
-    client_ip = Column('client_ip', String, nullable=False)
-    temp = Column('temp', String, nullable=False)
-    p = Column('p', String, nullable=False)
-    altitude = Column('altitude', String, nullable=False)
+    ssh_ip = Column('ssh_ip', String, nullable=False)
+    ssh_username = Column('ssh_username', String, nullable=False)
+    ssh_password = Column('ssh_password', String, nullable=False)
+    ssh_command = Column('ssh_command', String, nullable=False)
 
 
 class main(object):
@@ -98,16 +105,21 @@ class main(object):
     def run():
         parser = argparse.ArgumentParser()
         parser.add_argument("-s", "--setup", action="store_true", help="Setup Environment")
+        parser.add_argument("-a", "--add", action="store_true", help="Add Entry to Database")
         args = parser.parse_args()
 
         if args.setup:
+            PasswordHelper.setup_master_key()
             DatabaseHelper().create_tables()
             sys.exit(0)
 
+        if args.add:
+            DatabaseHelper().add_data()
+            sys.exit(0)
 
-        # Put this somewhere safe!
-
-
+        PasswordHelper.setup_master_key()
+        encoded = PasswordHelper.encode_password(input('Enter ssh_password:'))
+        print("Your pass = " + PasswordHelper.decode_password(encoded))
 
 
 if __name__ == "__main__":
--- ReadMe.md
+++ ReadMe.md
@@ -4,4 +4,4 @@ Securely runs commands over SSH
 ## Dependencies
 * Python 3
 * cryptography (pip install)
-* 
+* sqlalchemy
